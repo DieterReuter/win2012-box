d1edfc544318d349e66602841a11fc3b2aec8e14
Dieter Reuter <dieter.reuter@me.com>
2015-08-26 12:25:39 +0200

    Add Docker tools to provision step

--- scripts/provision.ps1
+++ scripts/provision.ps1
@@ -8,5 +8,9 @@ choco install -y git -params "/GitAndUnixToolsOnPath"
 choco install -y sublimetext2
 choco install -y sublimetext2.powershellalias
 
+# install Docker tools
+choco install -y docker -version 1.8.1
+choco install -y docker-machine -version 0.4.1
+
 # enable UAC
 Set-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\policies\system -Name EnableLUA -Value 1

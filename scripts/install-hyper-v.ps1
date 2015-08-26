c168bcf4c52c0a97ed05795df79c48d503fe1533
Dieter Reuter <dieter.reuter@me.com>
2015-08-26 11:35:51 +0200

    Fix typo

--- scripts/install-hyper-v.ps1
+++ scripts/install-hyper-v.ps1
@@ -1,2 +1,2 @@
-# install Hyper-V (note: it needs a restart, but we do it with `vagrant provison`)
+# install Hyper-V (note: it needs a restart, but we do it with `vagrant provision`)
 Install-WindowsFeature -Name Hyper-V -IncludeManagementTools # -Restart
